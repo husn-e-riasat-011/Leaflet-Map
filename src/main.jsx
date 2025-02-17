bc5e69f52304a16c6d61a713c37996f61f534b9e
Husn-e-Riasat <101085672+husn-e-riasat-011@users.noreply.github.com>
2025-02-17 14:18:21 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import EmployeeMap from "./LeafletMap";
+import EmployeeMap from "./leafletMap";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
     <EmployeeMap />
